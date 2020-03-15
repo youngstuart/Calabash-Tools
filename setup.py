92dd4dbf53be48b1e69a706150c4cb885b3b6104
= <youngstuart12@gmail.com>
2020-03-15 11:14:12 -0500

    Update function, currently extracts all. src extraction commented out

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from urllib.request import urlopen
+from zipfile import ZipFile
+from io import BytesIO
+
+# Dev paths, replace with following later
+#os.chdir(os.path.dirname(__file__))
+#cwd = os.getcwd()
+userHome = os.path.expanduser("~")
+cwd = os.path.join(userHome, 'AppData', 'Local','cb_temp')
+
+if not os.path.exists(cwd):
+    os.mkdir(cwd)
+
+def update():
+    repoUrl = "https://github.com/youngstuart/Calabash-Tools/archive/master.zip"
+
+    with urlopen(repoUrl) as repoArchive:
+        with ZipFile(BytesIO(repoArchive.read())) as zfile:
+            # extract everything and overwrite itself
+            zfile.extractall(cwd)
+            # If overwriting itself causes maya to crash, only extract src
+            # for fileName in zfile.namelist():
+            #     if fileName.startswith('Calabash-Tools-master/src'):
+            #         zfile.extract(fileName, cwd)
+            print(f'Calabash Tools extracted to:{cwd}')
+
+update()
+
